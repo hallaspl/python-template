dfb1be28c442a5d6cb7f481eff9f1e1c6a6edfa3
hallaspl <46907317+hallaspl@users.noreply.github.com>
2025-03-11 17:33:08 +0100

    Update test_build.sh

--- setup/test_build.sh
+++ setup/test_build.sh
@@ -5,8 +5,18 @@ else
    MESSAGE="[>>TestBuild<<]"
 fi
 
+echo "$MESSAGE source .venv/bin/activate"
+source .venv/bin/activate
+
+echo "$MESSAGE Using python:"
+which python
+python --version
+
 echo "$MESSAGE pip install -r requirements/development.txt"
-pip install -r requirements/development.txt
+python -m pip install -r requirements/development.txt
+
+echo "$MESSAGE pip install -r requirements/base.txt"
+python -m pip install -r requirements/base.txt
 
 echo "$MESSAGE pip install ."
-pip install -e .
+python -m pip install -e .
